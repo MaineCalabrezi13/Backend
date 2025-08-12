c79f02d9c070d04ddf05aa9030997e3acf38eedc
Maine Calabrezi <134637736+MaineCalabrezi13@users.noreply.github.com>
2025-08-11 21:55:30 -0300

    Add files via upload

---
+++ Lista 01/Exercicio-2/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nome = "Maine";
+        String cargo = "Projetista";
+        Double salario = 1900.00;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Salário: R$" + salario);
+    }
+}
