b95260b1f6270fde711b7a7a2ce6e89b305489a5
Maine Calabrezi <134637736+MaineCalabrezi13@users.noreply.github.com>
2025-08-18 11:18:07 -0300

    Delete Lista 01 directory

--- Lista 01/Exercicio-01/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    static String bemVindo = "Bem-vindo ao Sistema de Geração de Jogadores!";
-    public static void main(String[] args) {
-        System.out.println(bemVindo);
-    }
-}
