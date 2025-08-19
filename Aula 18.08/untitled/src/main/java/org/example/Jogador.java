bd877ffd4003fb62956030924ceeb5786f573834
Maine Calabrezi <134637736+MaineCalabrezi13@users.noreply.github.com>
2025-08-18 21:38:37 -0300

    Add files via upload

---
+++ Aula 18.08/untitled/src/main/java/org/example/Jogador.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+public class Jogador {
+    String nome;
+    String sobrenome;
+    String posicao;
+    String time;
+    int idade;
+
+    public Jogador(String nome, String sobrenome, String posicao, String time, int idade) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.posicao = posicao;
+        this.time = time;
+        this.idade = idade;
+    }
+}
